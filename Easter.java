9f49ca19a8b76be0f92a8b7f3683fbfdf331b43d
juniverse19 <ryanjkim18@gmail.com>
2025-09-05 14:43:26 +0000

    a

--- Easter.java
+++ Easter.java
@@ -1,10 +1,21 @@
 // Ryan Kim
 // This project will calculate the date easter falls on a given year
 
+import java.util.*;
+
 public class Easter {
     public static void main(String[] args) {
-        int y;
-        y= 1583
+        int y = 2025;
+        int a = y % 19;
+        int b = y/100;
+        int c = y % 100;
+        int d = b/4;
+        int e = b % 4;
+        int f = (b+8)/25;
+        int g = (b-f+1)/3;
+        int h = (19 * a + b - d - g +15) % 30;
+        
+        
         
     }
 }
